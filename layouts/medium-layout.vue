fd66e97c11ed605fbc343be82cc56d9223abcd8a
Eugen Preglej <ep53583@fer.hr>
2024-04-06 02:43:05 +0200

    first commit

---
+++ layouts/medium-layout.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- <nav class="left m l">
+        <a>
+            <i>home</i>
+            <div>Home</div>
+        </a>
+        <a>
+            <i>search</i>
+            <div>Search</div>
+        </a>
+        <a>
+            <i>share</i>
+            <div>share</div>
+        </a>
+    </nav> -->
+
+    <header class="s">
+        <nav>
+            <!-- <NavigationDrawer /> -->
+            <button class="transparent circle large s">
+                <i>arrow_back</i>
+            </button>
+            <h5 class="max left-align">Dashboard</h5>
+            <button class="transparent circle large">
+                <i class="large">logout</i>
+            </button>
+        </nav>
+    </header>
+    <slot />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<script>
+definePageMeta({
+    layout: "medium-layout",
+});
+</script>
+
+<template>
+    <main class="center-align max">
+        <table>
+            <thead class="transparent">
+                <tr>
+                    <th>Header</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>
+                        <button
+                            class="responsive small-elevate border large-padding vertical-padding"
+                        >
+                            <i class="large">home</i>
+                            <span class="large-text">Superhot VR</span>
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button class="responsive small-elevate vertical">
+                            <i>home</i>
+                            <span>Button</span>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <button
+            class="extend circle small-elevate extra medium-margin fixed right bottom"
+        >
+            <i>add</i>
+        </button>
+    </main>
+</template>
